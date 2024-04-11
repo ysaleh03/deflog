669e68960116f474f87a726571226f57f85b88dc
Oleg Yurchenko <oleg@bgmot.com>
2024-04-10 17:35:42 -0700

    Added printing and custom trie structure

--- src/actual_work_2.pl
+++ src/actual_work_2.pl
@@ -1,17 +1,21 @@
+:- use_module(library(apply)).
+:- table read_csv_and_insert/2.
+
 read_csv_and_insert(File, Trie) :-
     trie_new(Trie),
     csv_read_file(File, Rows),
-    read_csv_rows(Rows, Trie),
+    read_csv_rows(Rows, Trie).
     % writeln(UpdatedTrie). % Removed printing here
 
-read_csv_rows([], Trie, Trie).
+read_csv_rows([], Trie).
 read_csv_rows([Row|Rest], Trie) :-
     Row = row(Key, _, Value), % Assuming you want to use the first and third values
     insert_into_trie(Key, Value, Trie),
     read_csv_rows(Rest, Trie).
 
 insert_into_trie(Key, Value, Trie) :-
-    write(trie_gen(Trie, Key, The_value)),
+  %write(trie_gen(Trie, Key, The_value)),
+  %string_lower(UKey, Key), 
     ( trie_gen(Trie, Key, The_value) ->
       append(The_value, [Value], Result),
       trie_update(Trie, Key, Result)
@@ -19,17 +23,38 @@ insert_into_trie(Key, Value, Trie) :-
       trie_insert(Trie, Key, [Value])
     ).
 
-print_trie(Trie) :-
-    print_trie(Trie, "").
+%print_trie(Trie) :-
+%    print_trie(Trie, "").
+
+%print_trie(Trie, Prefix) :-
+%    trie_gen(Trie, Word, Data),
+%    string_concat(Prefix, Word, FullWord),
+%    %format('~s: ~w~n', [FullWord, Data]),
+%    %trie_get(Trie, Word, SubTrie),
+%    write(Data),
+%    print_trie(SubTrie, FullWord),
+%    false. % Force backtracking for multiple solutions
+%print_trie(_, _).
+
+get_all_sequences(Str, Strs) :-
+  string_chars('abcdefghijklmnopqrstuvwxyz', Chrs),
+  maplist(string_concat(Str), Chrs, Strs).
 
 print_trie(Trie, Prefix) :-
-    trie_gen(Trie, Word, Data),
-    atom_concat(Prefix, Word, FullWord),
-    format('~s: ~w~n', [FullWord, Data]),
-    trie_get(Trie, Word, SubTrie),
-    print_trie(SubTrie, FullWord),
-    false. % Force backtracking for multiple solutions
-print_trie(_, _).
+  trie_gen(Trie, Prefix, V),
+  write(V),
+  get_all_sequences(Prefix, Prefixes),
+  concurrent_maplist(print_trie(Trie), Prefixes),
+  false.
+print_trie(Trie,Prefix) :- 
+  \+ trie_gen(Trie, Prefix, _),
+  string_length(Prefix, N),
+  N < 4,
+  get_all_sequences(Prefix, Prefixes),
+  concurrent_maplist(print_trie(Trie), Prefixes),
+  false.
+print_trie(_,Prefix).
+
 
 
 % Usage: read_csv_and_insert('english_dictionary_2.csv', Trie), print_trie(Trie).
---
+++ src/trie_testing.pl
@@ -0,0 +1,103 @@
+% trie data structure will have:
+%   the current key (character)
+%   a list of children (trie, next character)
+%   a list of values associated with that key
+
+
+% root is a list of tries (we always use the root to add to the tree), Key already in root
+trie_insert(Tries, [KF|KR], V, NewTries) :-
+  member(trie(KF,_,_), Tries),
+
+  append(C1, [ModChild|C2], Tries),
+  trie_insert(ModChild, [KF|KR], V, UpdatedChild),
+  append(C1, [UpdatedChild|C2], NewTries).
+
+
+% root is a list of tries (we always use the root to add to the tree), Key not in root yet
+trie_insert(Tries, [KF|KR], V, NewTries) :-
+  is_list(Tries),
+  \+ member(trie(KF,_,_), Tries),
+
+  trie_insert(trie(KF,[],[]), [KF|KR], V, NewTrie),
+  append(Tries, [NewTrie], NewTries).
+
+% we reach where we want to insert
+trie_insert(trie(K, Children, Vals), [KF|KR], V, trie(K, Children, NewVals)) :-
+  K = KF,
+  length(KR, L),
+  L = 0,
+  
+  append(Vals, [V], NewVals).
+
+
+% The current value is equal to the key, length of the key is > 1, and next of key is a child of current node
+trie_insert(trie(K, Children, Vals), [KF|[KS|KR]], V, trie(K, NewChildren, Vals)) :-
+  K = KF,
+  length(KR, L),
+  L > 0,
+  member(trie(KS,_,_), Children),
+
+  append(C1, [ModChild|C2], Children),
+  trie_insert(ModChild, [KS|KR], V, UpdatedChild),
+  append(C1, [UpdatedChild|C2], NewChildren).
+
+% The current value is equal to the key, length of the key is > 1, and the next of key is not a child of current node
+trie_insert(trie(K, Children, Vals), [KF|[KS|KR]], V, trie(K, NewChildren, Vals)) :-
+  K = KF,
+  length(KR, L),
+  L > 0,
+  \+ member(trie(KS,_,_), Children),
+
+  trie_insert(trie(KS,[],[]), [KS|KR], V, NewTrie),
+  append(Children, [NewTrie], NewChildren).
+
+% the current value is equal to the key, length of the key is = 1, and the next of the key is a child of current node
+trie_insert(trie(K, Children, Vals), [KF|[KS|KR]], V, trie(K, NewChildren, Vals)) :-
+  K = KF,
+  length(KR, L),
+  L = 0,
+  member(trie(KS,_,_), Children),
+
+  append(C1, [ModChild|C2], Children),
+  trie_insert(ModChild, [KS|KR], V, UpdatedChild),
+  append(C1, [UpdatedChild|C2], NewChildren).
+
+% the current value is equal to the key, length of the key is = 1, and the next of the key is not a child of current node
+trie_insert(trie(K, Children, Vals), [KF|[KS|KR]], V, trie(K, NewChildren, Vals)) :-
+  K = KF,
+  length(KR, L),
+  L = 0,
+  \+ member(trie(KS,_,_), Children),
+
+  trie_insert(trie(KS,[],[]), [KS|KR], V, NewTrie),
+  append(Children, [NewTrie], NewChildren).
+
+read_csv_and_insert(File, Final_tries) :-
+  csv_read_file(File, Rows),
+  insert_csv_rows(Rows, [], Final_tries). % where '[]' is the new trie structure
+
+insert_csv_rows([], Trie, Trie).
+insert_csv_rows([Row|Rest_rows], Original_tries, Final_tries) :-
+  Row = row(Key, _, Value),
+  string_codes(Key, Char_list_key),
+  trie_insert(Original_tries, Char_list_key, Value, Intermediate_Tries),
+  insert_csv_rows(Rest_rows, Intermediate_Tries, Final_tries).
+
+print_tries(L, _) :-
+  is_list(L),
+  length(L, N),
+  N = 0.
+
+print_tries([trie(Key,Children,Values)|Rest], Layer) :-
+  char_code(C, Key),
+  maplist(write, Layer),
+  write(C),
+  write("\n"),
+  maplist(write, Layer),
+  write(Values),
+  write("\n"),
+
+  print_tries(Children, [" "|Layer]),
+  print_tries(Rest, Layer).
+
+
