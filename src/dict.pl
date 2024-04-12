90a39a9bb08b0b059d3992d7f7d29a270c6731a0
Youssef Saleh <ymseam@hotmail.com>
2024-04-11 19:41:02 -0700

    added main in dict.pl

---
+++ src/dict.pl
@@ -0,0 +1,16 @@
+% this is in prolog
+% TODO:
+% - Function that takes a string, searches trie for subtree starting.
+% - Function that takes a word,   searches trie for its definition. 
+
+:- [trie_testing].
+
+find_word(Trie, String, Definition) :- 
+  string_chars(String, Chars), find_word_helper(Trie, Chars, Definition).
+
+find_word_helper([trie(K, _, Def)|Tries], [Char], Definition) :- 
+  K = Char -> Def = Definition; find_word_helper(Tries, [Char], Definition). 
+
+find_word_helper([trie(K, Children, _)|Tries], [Char|Chars], Definition) :- 
+  K = Char -> find_word_helper(Children, Chars, Definition);
+  find_word_helper(Tries, [Char|Chars], Definition).
