e23684f6aaa0a8c64ab2068abd7031e9136e1947
Oleg Yurchenko <oleg@bgmot.com>
2024-04-11 21:50:27 -0700

    Updated python and dict

--- src/dict.pl
+++ src/dict.pl
@@ -8,21 +8,27 @@
 :- read_csv_and_insert('../data/english_dictionary_2.csv', Trie), nb_setval(trie, Trie).
 
 find_trie_node([trie(K, Children, Def)|Tries], [Char], [C|Word], Definition, Trie) :-
-  K = Char -> C = K, Def = Definition, trie(K, Children, Def) = Trie, Word = [];
-  find_trie_node(Tries, [Char], [C|Word], Definition, Trie).
+  (K = Char -> C = K, Def = Definition, trie(K, Children, Def) = Trie, Word = [];
+  find_trie_node(Tries, [Char], [C|Word], Definition, Trie)).
 
 find_trie_node([trie(K, Children, _)|Tries], [Char|Chars], [C|Word], Definition, TOut) :-
-  K = Char -> C = K,
+  (K = Char -> C = K,
               find_trie_node(Children, Chars, Word, Definition, TOut);
-  find_trie_node(Tries, [Char|Chars], [C|Word], Definition, TOut).
+  find_trie_node(Tries, [Char|Chars], [C|Word], Definition, TOut)).
+
+trie_to_words(trie(K, [], Def), Out) :-
+  (length(Def, L), L > 0 -> Out = [[K]];
+  Out = [[]]).
 
 trie_to_words(trie(K, Children, Def), Out) :-
+  length(Children, C),
+  C > 0,
   convlist(trie_to_words, Children, Outppp),
-  foldl(append, Outppp, [], Outpp),
-  maplist(append(K), Outpp, Outp),
+  foldl(append, Outppp, [[]], Outpp),
+  maplist(append([K]), Outpp, Outp),
   length(Def, L),
-  L > 0 -> Out = [[K]|Outp];
-  Out = Outp.
+  (L > 0 -> Out = [[K]|Outp];
+  Out = Outp).
 
 find_word(String, Definition) :- 
   string_codes(String, Chars),
@@ -32,8 +38,12 @@ find_word(String, Definition) :-
 find_preds(String, Word, Out) :-
   string_codes(String, Chars),
   nb_getval(trie, Trie),
-  find_trie_node(Trie, Chars, Word, _, TOut),
-  trie_to_words(TOut, Out).
+  find_trie_node(Trie, Chars, FWord, _, TOut),
+  length(I, 1),
+  append(Word, I, FWord),
+  trie_to_words(TOut, BOut),
+  maplist(append(Word), BOut, COut),
+  maplist(string_codes, Out, COut).
 
 % find_word_helper([trie(K, _, Def)|Tries], [Char], Definition) :- 
 %  K = Char -> Def = Definition;
